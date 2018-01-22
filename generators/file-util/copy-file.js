bd05f05989d210a7aeb80448c2064eba78d37834
risan <risanbagja@gmail.com>
2018-01-22 01:08:23 +0100

    :art: Apply prettier

--- generators/file-util/copy-file.js
+++ generators/file-util/copy-file.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 
-const copyFile = (source, destination) => new Promise((resolve, reject) => {
-  const readStream = fs.createReadStream(source);
-  const writeStream = fs.createWriteStream(destination);
+const copyFile = (source, destination) =>
+  new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(source);
+    const writeStream = fs.createWriteStream(destination);
 
-  readStream.pipe(writeStream);
+    readStream.pipe(writeStream);
 
-  readStream.on('error', err => reject(err));
-  writeStream.on('error', err => reject(err));
-  writeStream.on('close', resolve(true));
-});
+    readStream.on('error', err => reject(err));
+    writeStream.on('error', err => reject(err));
+    writeStream.on('close', resolve(true));
+  });
 
 module.exports = copyFile;
