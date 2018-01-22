b16e8af68e4cbd7487211206eb6ca7ba3d26cd45
risan <risanbagja@gmail.com>
2018-01-22 19:49:08 +0100

    :sparkles: Featured image, open graph and twitter card

--- config.js
+++ config.js
@@ -5,8 +5,36 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 module.exports = {
   // Site config
   url: 'http://localhost:8000',
-  siteDescription:
-    'Risan Bagja Pradana is an experienced Javascript and PHP developer',
+  title: 'Risan Bagja Pradana',
+  siteDescription: 'Tutorials, coding practice, and software craftmanship',
+  defaultMetaDescription: 'Risan Bagja Pradana is an experienced Javascript and PHP developer',
+
+  socialLinks: {
+    Github: 'https://github.com/risan',
+    Stackoverflow: 'https://stackoverflow.com/users/5138222',
+    Medium: 'https://medium.com/risan',
+    Twitter: 'https://twitter.com/risanbagja',
+    Email: 'mailto:risanbagja@gmail.com'
+  },
+
+  // Facebook Open Graph
+  openGraph: {
+    locale: 'en_US',
+    siteName: 'Risan Bagja Pradana',
+    defaultImage: {
+      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=1200&q=80',
+      width: 1200,
+      height: 800
+    }
+  },
+
+  // Twitter Card
+  twitterCard: {
+    type: 'summary_large_image',
+    site: '@risanbagja',
+    creator: '@risanbagja',
+    imageUrl: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=1200&q=80'
+  },
 
   // Where things are
   sourcePath: path.resolve(__dirname, 'src'),
