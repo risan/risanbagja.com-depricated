2689fa13169e54996890dbba137bf03b8a669c6b
risan <risanbagja@gmail.com>
2018-01-28 02:01:35 +0100

    :art: Remove console.log

--- src/assets/js/register-service-worker.js
+++ src/assets/js/register-service-worker.js
@@ -4,18 +4,9 @@ const registerServiceWorker = () => {
   }
 
   window.addEventListener('load', () => {
-    window.navigator.serviceWorker
-      /* eslint-disable no-undef */
-      .register(`${URL}service-worker.js`)
-      /* eslint-enable no-undef */
-      .then(registration =>
-        console.log(
-          `✅ Service worker is registered with scope: ${registration.scope}`
-        )
-      )
-      .catch(err =>
-        console.log(`🔥 Service worker registration failed: ${err.message}`)
-      );
+    /* eslint-disable no-undef */
+    window.navigator.serviceWorker.register(`${URL}service-worker.js`);
+    /* eslint-enable no-undef */
   });
 };
 
